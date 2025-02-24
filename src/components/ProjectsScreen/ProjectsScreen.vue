1425526d75651ec740635c261ba3f304fb10a281
slnnzmtl <slonanezametil@gmail.com>
2025-02-24 21:48:01 +0700

    feat: add ProjectsScreen component for projects section

---
+++ src/components/ProjectsScreen/ProjectsScreen.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import ProjectCard from './ProjectCard.vue'
+</script>
+
+<template>
+  <div class="projects-screen h-[100vh]">
+    <h1 class="projects-screen__title">Projects</h1>
+    <div class="projects-screen__cards">
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.projects-screen {
+  &__title {
+    text-align: center;
+    margin-bottom: 40px;
+  }
+
+  &__cards {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 20px;
+  }
+}
+</style>
---
+++ src/components/ProjectsScreen/index.ts
@@ -0,0 +1,2 @@
+export { default as ProjectsScreen } from './ProjectsScreen.vue'
+export { default as ProjectCard } from './components/ProjectCard.vue'
