8ada4c2175ed83ce5b9e5d305c83399d6368ffe4
slnnzmtl <slonanezametil@gmail.com>
2025-02-24 21:48:02 +0700

    feat: add view components for routing

---
+++ src/views/MainView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main-view">
+    <FaceScreen />
+    <ProjectsScreen />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { FaceScreen } from '../components'
+import { ProjectsScreen } from '../components';
+</script> 
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="projects-view">
+    <h1>Projects</h1>
+    <!-- Add your content here -->
+  </div>
+</template>
+
+<script setup lang="ts">
+</script> 
