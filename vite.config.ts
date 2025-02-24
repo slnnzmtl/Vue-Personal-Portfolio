eacb7f6afc78fc2cb88108d07e0f95f8721c181c
slnnzmtl <slonanezametil@gmail.com>
2025-02-24 21:47:35 +0700

    chore: initial project setup with Vue 3, TypeScript and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    tailwindcss(),
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@fontsource': fileURLToPath(new URL('./node_modules/@fontsource', import.meta.url))
+    }
+  }
 })
