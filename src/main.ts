510e7e9ad19a69f581ef35b7d0861a806799cf23
slnnzmtl <slonanezametil@gmail.com>
2025-02-24 21:47:50 +0700

    feat: add global styles and color variables

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
+import '@fontsource/fira-code/400.css' // Regular
+import '@fontsource/fira-code/500.css' // Medium
+import '@fontsource/fira-code/600.css' // Semi-bold
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
