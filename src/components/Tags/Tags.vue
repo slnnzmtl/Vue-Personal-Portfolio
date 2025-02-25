1284ac183ef7c77a254888f9ea065b0c0dd42e73
slnnzmtl <slonanezametil@gmail.com>
2025-02-26 02:15:20 +0700

    Update Tags component

---
+++ src/components/Tags/Tags.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tags">
+    <span 
+      v-for="tech in technologies" 
+      :key="tech" 
+      :class="['tag', { 'highlighted': selectedFilters.includes(tech) }]"
+    >
+      {{ tech }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'Tags',
+  props: {
+    technologies: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+    selectedFilters: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+
+.tags {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+}
+
+.tag {
+  display: inline-block;
+  background-color: rgba(255, 255, 255, 0.2);
+  color: var(--text);
+  border-radius: 4px;
+  padding: 0.25rem 0.5rem;
+  font-size: 0.875rem;
+
+  &.highlighted {
+    background-color: var(--cyan);
+    color: var(--background);
+  }
+}
+</style> 
