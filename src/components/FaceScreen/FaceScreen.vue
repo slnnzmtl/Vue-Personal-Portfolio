530288708cd4c78b54ff18c47fe55da4444aab9f
slnnzmtl <slonanezametil@gmail.com>
2025-02-25 12:51:36 +0700

    Refactor FaceScreen component to use Tailwind CSS for styling

--- src/components/FaceScreen/FaceScreen.vue
+++ src/components/FaceScreen/FaceScreen.vue
@@ -15,24 +15,34 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="face-screen flex items-center gap-18">
-    <Avatar />
+  <div class="face-screen flex items-center gap-6 sm:gap-18 p-4 pt-10 sm:pt-auto">
+    <Avatar class="max-w-[400px] max-h-[400px]" />
     <div class="flex flex-col gap-4 text-left">
-      <h2 class="text-6xl font-bold">{{ props.name }}</h2>
-      <p class="face-screen__position text-xl font-semibold">{{ props.position }}</p>
+      <h2 class="text-center sm:text-left text-4xl sm:text-5xl font-bold">{{ props.name }}</h2>
+      <p class="face-screen__position text-2xl font-semibold">{{ props.position }}</p>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .face-screen {
-  height: calc(100vh - 200px);
+  height: 800px;
 
   &__position {
     color: var(--text-secondary);
-    max-width: 300px;
+    max-width: 700px;
 
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    height: auto;
+
+    &__position {
+      max-width: 100%;
+      text-align: center;
+    }
   }
 }
 </style>
