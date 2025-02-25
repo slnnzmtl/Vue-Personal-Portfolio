0ab19766adcdb2200d2da8fe3a0fe69043e1bb3b
slnnzmtl <slonanezametil@gmail.com>
2025-02-25 19:23:23 +0700

    Update main application file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router'
 import '@fontsource/fira-code/400.css' // Regular
 import '@fontsource/fira-code/500.css' // Medium
 import '@fontsource/fira-code/600.css' // Semi-bold
-import './style.css'
+import './styles/style.css'
 
 const app = createApp(App)
 app.use(router)
