66c4472397c42ab8de17385fd444f583fe3a9cd0
slnnzmtl <slonanezametil@gmail.com>
2025-02-25 17:29:52 +0700

    Update component index and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@/*': fileURLToPath(new URL('./src/*', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
       '@fontsource': fileURLToPath(new URL('./node_modules/@fontsource', import.meta.url))
     }
   }
