0ee19849da2bc9b38b29040b0394c84ed1a38808
slnnzmtl <slonanezametil@gmail.com>
2025-02-25 16:55:37 +0700

    Refactor SkillsScreen and add SkillCard component

---
+++ src/components/SkillCard/SkillCard.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { Glass } from '@components/ui'; // Correct usage
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <Glass class="skill-card">
+    <h2 class="skill-card__title">{{ title }}</h2>
+    <div class="skill-card__content">
+      <slot></slot>
+    </div>
+  </Glass>
+</template>
+
+<style scoped lang="scss">
+.skill-card {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  padding: 1.5rem;
+
+
+  &__title {
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: var(--cyan);
+    margin-bottom: 1rem;
+  }
+
+  &__content {
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+
+      li {
+        color: var(--text);
+        margin-bottom: 0.5rem;
+        font-size: 1rem;
+        opacity: 0.8;
+        transition: opacity 0.3s ease;
+
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+  }
+}
+</style> 
---
+++ src/components/SkillsScreen/index.ts
@@ -0,0 +1,1 @@
+export { default as SkillsScreen } from './SkillsScreen.vue'
