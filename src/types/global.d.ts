c6b38d64c5c019bcc0f2ec1bfb4e8409ba333442
slnnzmtl <slonanezametil@gmail.com>
2025-02-26 12:53:27 +0700

    Update global type definitions

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,5 @@
-// Declare module for Vue single-file components
-declare module '*.vue' {
-  import { DefineComponent } from 'vue';
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
-
-// Declare any other global types or modules here 
