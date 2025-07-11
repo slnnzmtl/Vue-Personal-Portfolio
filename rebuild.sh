6c28e529f9a3936d12ceaf9851d4cf5b4b91b55c
slnnzmtl <slonanezametil@gmail.com>
2025-07-11 19:35:23 +0700

    feat: add deployment workflow and update rebuild script for Docker Compose

--- rebuild.sh
+++ rebuild.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+$stage = ${1}
+
 cd /root/containers/slnnzmtl-xyz || {
     echo "❌ Failed to change to project directory"
     exit 1
 }
 
+$docker_compose_file = "docker-compose.yml" 
+
+if [ "$stage" == "dev" ]; then
+    $docker_compose_file = "docker-compose.dev.yml"
+fi
+
 echo "🔄 Starting Docker Compose rebuild process..."
 
 echo "📦 Stopping and removing containers..."
-docker compose down
+docker compose -f $docker_compose_file down
 
 echo "🔨 Building images without cache..."
-docker compose build --no-cache
+docker compose -f $docker_compose_file build --no-cache
 
 echo "🚀 Starting containers in detached mode..."
-docker compose up -d
+docker compose -f $docker_compose_file up -d
 
 echo "✅ Docker Compose rebuild completed successfully!"
